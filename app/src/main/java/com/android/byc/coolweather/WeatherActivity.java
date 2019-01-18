a680af1dd78d48a095491cb57811016f8a90de9a
yumengqi <yumengqi@example.com>
2019-01-18 17:34:19 +0800

    增加功能

--- app/src/main/java/com/android/byc/coolweather/WeatherActivity.java
+++ app/src/main/java/com/android/byc/coolweather/WeatherActivity.java
@@ -158,7 +158,7 @@ private Button navButton;
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败",
                                     Toast.LENGTH_SHORT).show();
                         }
-                        //swipeRefresh.setRefreshing(false);
+                        swipeRefresh.setRefreshing(false);
                     }
                 });
 
@@ -173,6 +173,7 @@ private Button navButton;
                                 Toast.LENGTH_SHORT).show();
                         swipeRefresh.setRefreshing(false);
                     }
+
 
                 });
             }
